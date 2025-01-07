6cb44e0f13691b9bccfa3c0af33df333e9d4a672
Koozie24 <jake.mckenzie2017@gmail.com>
2025-01-07 11:53:00 -0500

    condensed logic of single input cleared newline left in input buffer after valid input of single character fixed getline reading newline and skipping input

--- main.cpp
+++ main.cpp
@@ -12,15 +12,10 @@ int take_single_int_input(int number_of_options){
     int single_input;
     for(;;){
         std::cin >> single_input;
-        if(number_of_options == 3){ //for sign in with 3 choices
-            if(single_input >= 1 && single_input <= 3){
-                break;
-            }
-        }
-        if(number_of_options == 4){ //for home page with 4 choices
-            if(single_input >=1 && single_input <= 4){
-                break;
-            }
+        if((number_of_options== 3 && single_input >= 1 && single_input <= 3) || //3 option
+        (number_of_options == 4 && single_input >=1 && single_input <= 4)){ // 4 options
+            std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n'); //clear input buffer for getline
+            break;
         }
         std::cout << "Sorry, that option is not valid. Try again from the options listed: ";
         std::cin.clear(); //reset cin state
@@ -30,20 +25,32 @@ int take_single_int_input(int number_of_options){
     return single_input;
 }
 
+std::string take_string_input(){
+    for(;;){
+        std::string user_input;
+        getline(std::cin, user_input);
+
+        std::cout << user_input;
+
+        return user_input;
+    }
+}
+
 int main(){
     
     Session session("tcp://127.0.0.1:3306", "passmateadmin", "D1774%!f71pG", "passmate"); //initialize session
-    
+
     bool current_session = false;
     display_sign_in_screen(); //show sign in/create account options
     int sign_in_screen_command = take_single_int_input(3); //take user command
-
+    std::string cmd;
     switch(sign_in_screen_command){ //handling sign in screen
         case(1): //create account
             std::cout << "Creating an account.. " << std::endl; 
             break; 
         case(2): //account sign in
-            std::cout << "Signing into an account.. " << std::endl; 
+            std::cout << "Signing into an account.. " << std::endl;
+            cmd = take_string_input();
             current_session = true;
             break;
         case(3): //testing  advance to home screen will change to exit program
