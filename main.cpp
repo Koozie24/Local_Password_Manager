32d8a702113f1c9295aae6e82f34a3a92ba7d136
Koozie24 <jake.mckenzie2017@gmail.com>
2025-01-13 14:37:35 -0500

    added email/pw/username validation and account creation

--- main.cpp
+++ main.cpp
@@ -49,6 +49,8 @@ std::unique_ptr<sql::ResultSet> get_username(const std::string &search_name, std
     return nullptr;
 }
 
+/*Takes four string arguments for account infomration, and a shared pointer to a connection to sql database. 
+Tries to insert prepared statement to DB and outputs the error if unsuccessful.*/
 void create_new_user(std::shared_ptr<sql::Connection> conn, const std::string &user_name, const std::string &password_hash, const std::string &salt, const std::string &email){
     try{
         std::unique_ptr<sql::PreparedStatement> pstmt(conn->prepareStatement("INSERT INTO users ('username', 'email', 'password_hash', 'salt) VALUES (?, ?, ?, ?)"));
@@ -62,6 +64,106 @@ void create_new_user(std::shared_ptr<sql::Connection> conn, const std::string &u
         std::cerr << e.what() << std::endl;
     }
 
+}
+
+bool check_password_complexity(const std::string& password){
+    bool number = false;
+    bool upperletter = false;
+    bool lowerletter = false;
+    bool special_char = false;
+    int length_of_pw = password.length();
+    bool requirements_met = false;
+
+     for(char c : password){
+        //convert current char to ordinal value and check against ranges of values if given flag is false
+        int ordinal_value_of_char = static_cast<int>(c);
+
+        if(ordinal_value_of_char >= 97 && ordinal_value_of_char <= 122 && lowerletter == false){
+            lowerletter = true;
+        }
+        if(ordinal_value_of_char >= 65 && ordinal_value_of_char <= 90 && upperletter == false){
+            upperletter = true;
+        }
+        if(ordinal_value_of_char >= 48 && ordinal_value_of_char <= 57 && number == false){
+            number = true;
+        }
+        if(ordinal_value_of_char >= 32 && ordinal_value_of_char <= 47 && special_char == false){
+            special_char = true;
+        }
+     }
+
+     if(number == true && upperletter == true && lowerletter == true && special_char == true && length_of_pw >= 10){
+        requirements_met = true; //password requirements are met
+     }
+
+     return requirements_met;
+}
+
+bool check_valid_email(const std::string& email){
+    bool is_valid_email = false;
+    bool contain_at_symbol = false;
+    bool contains_valid_dot = false;
+
+    for(char c : email){
+        if(c == '@'){ 
+            contain_at_symbol = true;
+            break;
+        }
+    }
+
+    int string_length = email.length();
+    if(string_length >= 7){ //check for minimum length of email
+        int last_index = string_length - 4; //acount for .com or .net etc
+        std::string domain = email.substr(last_index, 4); //get 4 letter substring from given index
+
+        std::cout <<domain << std::endl;
+        if(domain == ".com" || domain == ".net" || domain == ".org"){ //if this domain
+            contains_valid_dot = true;
+        }
+    }
+
+    if(contains_valid_dot == true && contain_at_symbol == true){
+        is_valid_email = true;
+    }
+
+    return is_valid_email;
+}
+
+/*function that takes inputs from user to create a new account. takes an argument of a reference to a session object
+Return a vector of strings if email is valid and password meets complexity and no account with username exists*/
+std::vector<std::string> create_new_account(Session& session){
+    std::vector<std::string> account_details;
+    bool meets_input_requirements = false;
+    while(!meets_input_requirements){ //get account information from user while requiremnets arent met
+        std::string new_user, passcode, new_email;
+        bool password_req = false;
+        bool email_req = false;
+        bool username_is_unique = false;
+        bool is_an_email = false;
+        std::shared_ptr<sql::ResultSet> check_user;
+
+        std::cout << "Username: ";
+        new_user = take_string_input(); // check username now in DB
+        check_user = get_username(new_user, session.getConnection());
+
+        std::cout << "Password: ";
+        passcode = take_string_input(); //get inputted password
+        password_req = check_password_complexity(passcode); //check for pw complexity
+
+        std::cout << "Email: ";
+        new_email = take_string_input();
+        email_req = check_valid_email(new_email);//check contains @ symbol and ends with .com/.net/.org/.gov
+
+        if(password_req == true && email_req == true && !check_user->next()){ //check bools true and ther are no rows (didnt return a user)
+            meets_input_requirements = true;
+            account_details.push_back(new_user);
+            account_details.push_back(passcode);
+            account_details.push_back(new_email);
+            break;
+        }
+    }
+
+    return account_details;
 }
 
 int main(){
@@ -80,22 +182,10 @@ int main(){
         switch(start_screen_command){ //handling sign in screen
             case(1): {//create account
                 std::cout << "Creating an account.. " << std::endl; 
+                bool meets_input_requirements = false;
+                std::vector<std::string> account_details = create_new_account(session); //call function to get a vector of strings containing inputs
 
-                std::cout << "Username: " << std::endl;
-                std::string new_user = take_string_input();
-                std::cout << "password: " << std::endl;
-                std::string passcode = take_string_input();
-                std::cout << "email: " <<std::endl;
-                std::string new_email = take_string_input();
-
-                User new_user_account = User(new_user, passcode, new_email);
-
-                //std::cout << new_user_account.getUsername() << std::endl;
-                //std::cout << new_user_account.getEmail() << std::endl;
-                std::cout << new_user_account.getSalt() << std::endl;
-                //std::cout << new_user_account.getPassword() << std::endl;
-
-                take_string_input();
+                User new_user_account = User(account_details[0], account_details[1], account_details[2]); //create user object using account details
 
                 break; 
             }
